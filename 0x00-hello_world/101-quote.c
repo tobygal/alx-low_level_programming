edcb613ba139d983c9886c6e32784e0d60223f93
tobygal <tochiazolike@yahoo.com>
2022-03-10 15:42:34 -0800

    Displaying into stderr in C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,27 +3,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-/* 
+/**
  * main - Entry point
  *
- * Return: Always return 0
+ * Return: Always return 1
  */
 
 int main(void)
 {
-	int fd[2];
-	char sbuf1[58] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
-	char sbuf2[58];
-
-	/* use open function to access the buffer*/
-	fd[0] = open("task8.txt", O_RDWR | O_CREAT);
-	fd[1] = open("task8.txt", O_RDWR | O_CREAT);
-
-	write(fd[0], sbuf1, strlen(sbuf1));
-	write(1, sbuf2, read(fd[1], sbuf2, 58));
-
-	close(fd[0]);
-	close(fd[1]);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
