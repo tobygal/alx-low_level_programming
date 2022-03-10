0edfc86355c64f86c99a5a326e13ed8a2a198929
tobygal <tochiazolike@yahoo.com>
2022-03-10 09:00:36 -0800

    Script to run C through preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char str1[51];
 
-	strcpy(str1, "Programming is like building a multilingual puzzle");
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
 
 	puts(str1);
 
