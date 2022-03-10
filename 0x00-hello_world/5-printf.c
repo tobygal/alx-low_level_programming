380525e78925be4754182d28bfe6ebfa2300e1d0
tobygal <tochiazolike@yahoo.com>
2022-03-10 09:23:15 -0800

    Script to run C through preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,14 @@
 #include <stdio>
 
-/*
- * Main
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
