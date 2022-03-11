519d4ecf3077d39d9a3986d9075050e27ad798c2
tobygal <tochiazolike@yahoo.com>
2022-03-11 10:03:27 -0800

    Alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
