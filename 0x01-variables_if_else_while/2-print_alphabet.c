725d87f8dccba99ee2d789ecf4205aecec01e709
tobygal <tochiazolike@yahoo.com>
2022-03-11 07:16:50 -0800

    Printing of lower case alphabets in C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always retun 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= z)
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
