b833d15dffdaf6809c69e11babf0593f73979516
tobygal <tochiazolike@yahoo.com>
2022-03-15 15:15:39 -0700

    natural number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - Print numbers to 98 both left and right
@@ -8,26 +9,17 @@
 
 void print_to_98(int n)
 {
-	if (n > 98)
+	while (n != 98)
 	{
-		_putchar(n);
-		n--;
-		if (n == 98)
+		printf("%i, ", n);
+		if (n > 98)
 		{
-			break;
+			n--;
 		}
-		_putchar(',');
-		_putchar(' ');
-	}
-	else if (n <= 98)
-	{
-		_putchar(n);
-		n++;
-		if (n == 98)
+		else
 		{
-			break;
+			n++;
 		}
-		_putchar(',');
-		_putchar(' ');
 	}
+	printf("98\n");
 }
