e196097a8190678245016f6628f50440ff2a66b5
tobygal <tochiazolike@yahoo.com>
2022-03-15 09:51:36 -0700

    Function pto print alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet
+ *
+ */
+
+void print_alphabet(void)
+{
+	int alp;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+
+	}
+	putchar('\n');
+}
