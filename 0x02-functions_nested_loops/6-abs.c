aa25eb3f5a870e3be452ad3e958773a8e7946e51
tobygal <tochiazolike@yahoo.com>
2022-03-15 13:51:27 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute values of numbers
+ *
+ * Return: the absolute value
+ * @int: a number
+ */
+
+
+int _abs(int);
+{
+	return (v * ((v > 0) - (v < 0)));
+}
