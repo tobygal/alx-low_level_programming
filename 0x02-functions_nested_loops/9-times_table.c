843292f31ee7598d9f1f23f1e789a25e969f1a95
Tochi <Tochiazolike@yahoo.com>
2022-03-16 09:58:32 +0100

    using for loop to display 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,13 +11,9 @@ void times_table(void)
 	int b;
 	int c;
 
-	a = 0;
-
-	while (a <= 9)
+	for (a = 0; a <= 9; a++)
 	{
-		b = 0;
-
-		while (b <= 9)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
 
@@ -31,8 +27,8 @@ void times_table(void)
 				if (b != 0)
 				{
 					_putchar(' ');
-					_putchar(c + '0');
 				}
+				_putchar(c + '0');
 			}
 			if (b == 9)
 			{
@@ -40,9 +36,7 @@ void times_table(void)
 			}
 			_putchar(',');
 			_putchar(' ');
-			b++;
 		}
 		_putchar('\n');
-		a++;
 	}
 }
