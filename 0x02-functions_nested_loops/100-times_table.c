1d6bbeb1881fddfd7d2594e30e54c2a3125c195d
tobygal <tochiazolike@yahoo.com>
2022-03-16 09:36:07 -0700

    Multiply n numbers

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_times_table - display n x table
+ * @n: maximum number to be multiplied
+ */
+
+void print_times_table(int n)
+{
+	int a;
+	int b;
+	int c;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+
+				if (c > 9)
+				{
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else
+				{
+					if (b != 0)
+					{
+						_putchar(' ');
+					}
+					_putchar(c + '0');
+				}
+				if (b == n)
+				{
+					break;
+				}
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
