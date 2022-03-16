fa4b0f97630a2531f8b784ab41817a8c2f0b2394
tobygal <tochiazolike@yahoo.com>
2022-03-15 18:14:02 -0700

    commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - display 9 x table
+ *
+ */
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	a = 0;
+
+	while (a <= 9)
+	{
+		b = 0;
+
+		while (b <= 9)
+		{
+			c = a * b;
+
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else
+			{
+				_putchar(c + '0');
