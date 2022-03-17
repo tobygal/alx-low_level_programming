06f54b48336e0683d42136c13463d33d30c00532
tobygal <tochiazolike@yahoo.com>
2022-03-17 16:01:37 -0700

    Prime factor of a number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,7 +10,7 @@ int main(void)
 	unsigned long int a, i;
 
 	a = 612852475143;
-	for (1 = 3; i < (a / 2); i = i + 2)
+	for (i = 3; i < (a / 2); i = i + 2)
 	{
 		while ((a % i == 0) && (a != i))
 		{
