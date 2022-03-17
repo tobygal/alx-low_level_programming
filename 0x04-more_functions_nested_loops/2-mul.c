97f91c86d237dbbc3dec7987326048aa32f5bbd8
tobygal <tochiazolike@yahoo.com>
2022-03-17 07:30:30 -0700

    Changes to print numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int mul - function to multiply two integers
+ * mul - function to multiply two integers
  * @a: The first integer to be multiplied
  * @b: The second integer to be multiplied
  * Return: return the product
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,7 +14,7 @@ void print_most_numbers(void)
 		{
 			continue;
 		}
-		_putchar((i % 10) + '0');
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
