6c229e12aaed3e0f204291a38697df4afcd59ded
Tochi <Tochiazolike@yahoo.com>
2022-03-22 00:25:58 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,14 @@
 void puts2(char *str)
 {
     int i = 0;
+    
     while (str[i] != '\0')
     {
-         if (i % 2 == 0)
-         {
-              _putchar(str[i]);
-          }
-      i++;
-      }
-      _putchar('\n');
+        if (i % 2 == 0)
+        {
+            _putchar(str[i]);
+        }
+        i++;
+    }
+    _putchar('\n');
 }
