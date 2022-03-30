49e17441d94c672549b89ee1c25d56b407771aac
tobygal <tochiazolike@yahoo.com>
2022-03-30 16:48:35 -0700

    corrections

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,12 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		write(1, &*s, 1);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
 	{
-		write(1, '\n', 1);
+		_putchar('\n');
 	}
 }
 
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,6 +11,6 @@ void _print_rev_recursion(char *s)
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		write(1, &*s, 1);
+		_putchar(*s);
 	}
 }
