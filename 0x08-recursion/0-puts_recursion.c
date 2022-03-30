8984d999a02a685a39890edfd26456dc87b0e992
tobygal <tochiazolike@yahoo.com>
2022-03-30 14:38:08 -0700

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+void _puts_recursion(char *s)
+{
+	if (s = '\0')
+		return 
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number whose square root is to be found
+ * Return: return the natural square or -1 if no natural  square
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (
