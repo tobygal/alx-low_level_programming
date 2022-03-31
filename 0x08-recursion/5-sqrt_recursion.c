563f8322fcaf0aaf71650e3a7d87a575505f8043
tobygal <tochiazolike@yahoo.com>
2022-03-30 17:09:18 -0700

    Squareroot using recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,6 +5,7 @@
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number whose square root is to be found
  * Return: return the natural square or -1 if no natural  square
+ * @i: the integers to be checked as possible sqrt
  */
 int _sqrt(int n, int i);
 
