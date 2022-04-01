45ad2055e2179d4b247bb3949d69856e381f128f
tobygal <tochiazolike@yahoo.com>
2022-04-01 08:40:51 -0700

    Addition of numbers passed through argument

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always return 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (1);
+
+}
