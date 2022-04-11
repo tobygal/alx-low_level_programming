f86f951cc197647696456dea8517d83e0a7a6de9
tobygal <tochiazolike@yahoo.com>
2022-04-11 14:04:23 -0700

    2-print_dog.c creation

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog tye
+ */
+
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n",
+				(*d).name != NULL ? d->name : "(nil)",
+				(*d).age,
+				(*d).owner != NULL ? d->owner : "(nil)";
+	}
+}
