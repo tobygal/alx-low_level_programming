d3637bc9ef1ef0681ee7c9cd6f8be6d92976251c
tobygal <tochiazolike@yahoo.com>
2022-04-11 10:05:28 -0700

    creation of dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure called dog
+ * @name: a member of char datatype
+ * @age: a member of float datatype
+ * @owner: a member of char datatype
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
