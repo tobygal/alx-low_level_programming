93497a83e6ea52149a51f7d92bdaee71b7c57560
tobygal <tochiazolike@yahoo.com>
2022-04-13 10:25:05 -0700

    0-print_name.c creation

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name
+ * @name: name to be printed
+ * @f: funtion pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+ALX task for project function pointers in C
