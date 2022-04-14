c0809a8094a7efe34a103bad5cb5a11282b57add
tobygal <tochiazolike@yahoo.com>
2022-04-13 17:39:42 -0700

    4th amendment of 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - function to select the correct function to perform
@@ -24,7 +25,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (*s == *(ops[i].op))
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		++i;
 	}
