2bc7fa61e079b43ebdca177b19f00310de479b98
tobygal <tochiazolike@yahoo.com>
2022-04-13 18:30:56 -0700

    100-main_opcodes.c creation

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int bytes;
+	char *op;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (i < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	op = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", op[i]);
+			break;
+		}
+		printf("%02hhx ", op[i]);
+	}
+	return (0);
+}
