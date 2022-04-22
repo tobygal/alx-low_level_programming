61beb7cbfd29ddff8e768f0efcc706e604c90c49
tobygal <tochiazolike@yahoo.com>
2022-04-21 19:55:58 -0700

    1-list_len.c created

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - tranverses through a linked list
+ * @h: the linked list
+ * Return: return the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+	const list_t current = h;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		nodes++;
+	}
+	return (nodes);
+}
