a0c4049d680c735fd6a81b5c3b71022d8b902112
tobygal <tochiazolike@yahoo.com>
2022-04-26 05:32:59 -0700

    delete update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int count = 0, i = 0;
 	listint_t *current, *temp, *old;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 	current = old = *head;
 	while (current != NULL)
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(old);
 		return (1);
 	}
-	if (i < index - 1)
+	while (i < index - 1)
 	{
 		old = old->next;
 		i++;
