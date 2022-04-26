ec9669ffb987a89024669c748c959e170549efa1
tobygal <tochiazolike@yahoo.com>
2022-04-26 03:36:38 -0700

    task9 new update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		count++;
 	}
-	if (idx >= count)
+	if (idx > count)
 	{
 		free(newnode);
 		return (NULL);
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = newnode;
 		return (newnode);
 	}
-	while (i < idx)
+	while (i < idx - 1)
 	{
 		temp = temp->next;
 		i++;
