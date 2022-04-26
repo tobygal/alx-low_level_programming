56059304d95208f4e599f7508734c426981426f6
tobygal <tochiazolike@yahoo.com>
2022-04-26 03:32:11 -0700

    task9 update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = newnode;
 		return (newnode);
 	}
-	while (i < idx - 1)
+	while (i < idx)
 	{
 		temp = temp->next;
 		i++;
