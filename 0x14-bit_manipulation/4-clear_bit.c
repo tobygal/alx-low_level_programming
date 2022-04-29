cc99190b81dfdcd5dce43d854e697204010a8bdf
tobygal <tochiazolike@yahoo.com>
2022-04-28 20:12:15 -0700

    4-clear_bit.c update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,6 +14,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 	num = ~(1 << index);
-	*n = *n & set;
+	*n = *n & num;
 	return (1);
 }
