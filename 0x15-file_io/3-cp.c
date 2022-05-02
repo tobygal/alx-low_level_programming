1eeee8281efd83ba1a8c1ea5644d89343d0a9d6e
tobygal <tochiazolike@yahoo.com>
2022-05-02 14:46:47 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,15 +25,19 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fdto = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	status = read(fdfr, buf, MAXSIZE);
-	if (status > 0)
+	/*status = read(fdfr, buf, MAXSIZE);*/
+	while ((status = read(fdfr, buf, MAXSIZE)) > 0)
 	{
-		dest = write(fdto, buf, status);
-		if (dest == -1)
+		if (fdto < 0 || (write(fdto, buf, status) != status))
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
+	}
+	if (status < 0)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	status = close(fdfr);
 	if (status == -1)
