7656f26e20c7407181b5a4ba0c554b3d45cc066a
tobygal <tochiazolike@yahoo.com>
2022-07-11 03:04:02 -0700

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,15 +20,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	new->n = n;
 	new->prev = NULL;
-	if (head == NULL)
-	{
-		new->next = NULL;
-		*head = new;
-	}
-	else
-	{
-		new->next = *head;
-		*head = new;
-	}
+	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+
 	return (new);
 }
